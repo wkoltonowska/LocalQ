14848c0a0eb61d831d5081147f95d08206db0429
Weronika Kołtonowska <wkoltonowska98@gmail.com>
2025-01-15 22:21:20 +0100

    add project

---
+++ app/src/main/java/com/example/localq/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.localq
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.localq.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+private lateinit var binding: ActivityMainBinding
+private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+          enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+
+//    binding.startbtn.setOnClickListener{
+//        navController.navigate(R.id.mainActivity2_to_firstQuestoinPzn3)
+//    }
+
+
+    }
+}
